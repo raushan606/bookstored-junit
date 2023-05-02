d52238680c93bd813905f01a659c0e72ed0c442d
Raushan <raushankumar606@gmail.com>
2023-05-02 13:28:40 +0530

    page 35

--- src/test/java/bookstored/BookShelfSpec.java
+++ src/test/java/bookstored/BookShelfSpec.java
@@ -1,15 +1,22 @@
 package bookstored;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.example.BookShelf;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
-@DisplayName("<= BookShelf Specification =>")
+@DisplayName("A BookShelf")
 public class BookShelfSpec {
 
     private BookShelfSpec(TestInfo testInfo) {
@@ -19,10 +26,47 @@ public class BookShelfSpec {
     @Test
     @DisplayName("is empty when no book is added to it")
     public void shelfEmptyWhenNoBooksAdded(TestInfo testInfo) throws Exception {
-        System.out.println("Working on test " + testInfo.getDisplayName());
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+
+    @Test
+    public void nullAssertionTest() {
+        String str = null;
+        assertNull(str);
+        assertNull(str, "str should be null");
+        assertNull(str, () -> "str should be null");
+    }
+
+    @Test
+    void shouldCheckForEvenNumbers() {
+        int number = new Random(10).nextInt();
+        assertTrue(() -> number % 2 == 0, number + " is not an even number.");
+
+        BiFunction<Integer, Integer, Boolean> divisible = (x, y) -> x % y == 0;
+        Function<Integer, Boolean> multipleOf2 = (x) -> divisible.apply(x, 2);
+        assertTrue(() -> multipleOf2.apply(number), () -> " 2 is not factore of " + number);
+
+        List<Integer> numbers = Arrays.asList(1, 1, 1, 1, 2);
+        assertTrue(() -> numbers.stream().distinct().anyMatch(BookShelfSpec::isEven), "Did not find an even number in the " +
+                "list");
+    }
+
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    @Test
+    void thisTestShouldFail() {
+        fail(() -> "This test should fail");
+    }
+
+    @Test
+    public void shelfToStringShouldPrintBookCountAndTitles() throws Exception {
+        BookShelf shelf = new BookShelf();
+        List<String> books = shelf.books();
+        shelf.add("The Phone Book");
     }
 
 }
